a8e2d19f06fa44cc6d6a63590385987e95de9c1a
YongdaeKim <yongdae91@hanmail.net>
2016-02-10 13:31:25 +0900

    공통코드 테이블에 지역코드 추가

--- app/models/common_code.rb
+++ app/models/common_code.rb
@@ -1,5 +1,17 @@
 class CommonCode < ApplicationRecord
   def self.find_by_party(party_id)
-    where(main_id: 'PARTY').where(detail_id: party_id)
+    party_list.where(detail_id: party_id)
+  end
+
+  def self.party_list
+    where(main_id: 'PARTY')
+  end
+
+  def self.find_by_region(region_id)
+    region_list.where(detail_id: region_id)
+  end
+
+  def self.region_list
+    where(main_id: 'REGION')
   end
 end
--- db/seeds/01_common_code.rb
+++ db/seeds/01_common_code.rb
@@ -19,4 +19,91 @@ CommonCode.create(
   detail_id: 4,
   code_name: '무소속'
 )
+
 # 지역 코드
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 1,
+  code_name: '서울'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 2,
+  code_name: '인천'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 3,
+  code_name: '대전'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 4,
+  code_name: '부산'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 5,
+  code_name: '광주'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 6,
+  code_name: '대구'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 7,
+  code_name: '울산'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 8,
+  code_name: '경기'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 9,
+  code_name: '강원'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 10,
+  code_name: '충남'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 11,
+  code_name: '충북'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 12,
+  code_name: '전남'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 13,
+  code_name: '전북'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 14,
+  code_name: '경남'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 15,
+  code_name: '경북'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 16,
+  code_name: '제주'
+)
+CommonCode.create(
+  main_id: 'REGION',
+  detail_id: 17,
+  code_name: '비례대표'
+)
+
